f365acdc5bc23c171ef0d17c1e9c5d49a299b214
wurbanski-del <w.urbanski@escola.pr.gov.br>
2026-06-22 15:19:47 +0000

    .

--- for.js
+++ for.js
@@ -2,4 +2,7 @@ const numeros = {100, 200, 300, 400, 500, 600}
 
 for (let indice = 0; indice < 6; indice++){
     console.log("oi");
-}
+}//terceira exprecao : condicao 
+
+
+for (last indice = 0; é 
---
+++ media-for.js
@@ -0,0 +1,1 @@
+const notas = {10, }
