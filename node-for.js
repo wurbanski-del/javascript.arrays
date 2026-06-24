d768ae6783798381e597720de2a807bf84bc5b36
wurbanski-del <w.urbanski@escola.pr.gov.br>
2026-06-24 15:08:35 +0000

    .

---
+++ node-for.js
@@ -0,0 +1,17 @@
+const notas = {10, 6.5, 8, 7.5};
+
+//const media = (notas{0} + notas{1} + notas{2} + notas{3}) / notas.length;
+
+let somaDasNotas = 0;
+
+for (let i = 0; i < notas.legth; i++){
+
+    somaDasNotas += notas{i};
+
+}
+
+const media = somaDasNotas / notas.length;
+
+console.log('A media Das Notas é ${media},');
+
+
