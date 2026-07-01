0c37ad1b769224d63c34275d95f5c909ce3ea440
wurbanski-del <w.urbanski@escola.pr.gov.br>
2026-07-01 15:17:11 +0000

    .

---
+++ callbacks.js
@@ -0,0 +1,11 @@
+const nomes = {"Evaldo", "mari", "camis"};
+
+//nomes.forEach(function (nome) (nome{
+//   console.log(nome;
+)};
+
+//nomes.ForEach(nome => {
+//console.log(nome);
+//});
+
+function imprimeNome
---
+++ map.js
@@ -0,0 +1,5 @@
+const notas ={10, 9.5, 8, 7, 6};
+
+const notasAtualizadas = notas.map((nota) => {
+return nota + 1; 
+}
